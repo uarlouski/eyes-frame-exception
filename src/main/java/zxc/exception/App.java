728df5930f164d0fc3991ff4fa3fe44af601d378
Uladzislau Arlouski <uarlouski@gmail.com>
2019-10-01 11:25:53 +0300

    Fix to use element region

--- src/main/java/zxc/exception/App.java
+++ src/main/java/zxc/exception/App.java
@@ -3,10 +3,12 @@ package zxc.exception;
 import java.util.function.BiConsumer;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.applitools.eyes.CoordinatesType;
 import com.applitools.eyes.Region;
 import com.applitools.eyes.StdoutLogHandler;
 import com.applitools.eyes.selenium.Eyes;
@@ -22,7 +24,11 @@ public class App
         {
             wd.get("http://www.meteo.psu.edu/~jyh10/classes/meteo473/basic-frames.html");
             wd.switchTo().frame((WebElement) wd.findElement(By.xpath("/html/frameset/frame[3]")));
-            eyes.check("", Target.region(Region.EMPTY));
+            WebElement target = wd.findElement(By.xpath("/html/body/font/p[1]"));
+            Rectangle rect = target.getRect();
+            Region region = new Region(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight(),
+                    CoordinatesType.CONTEXT_RELATIVE);
+            eyes.check("element", Target.region(region));
         });
     }
 
